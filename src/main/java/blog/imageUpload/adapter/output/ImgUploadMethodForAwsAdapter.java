ee0148d291c507d286c344055808a7793fbe0126
717276 <limgh04@gamil.com>
2023-12-08 18:55:09 +0900

    commit

--- src/main/java/blog/imageUpload/adapter/output/ImgUploadMethodForAwsAdapter.java
+++ src/main/java/blog/imageUpload/adapter/output/ImgUploadMethodForAwsAdapter.java
@@ -7,7 +7,6 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
